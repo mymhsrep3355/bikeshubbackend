a1a358323bafe15684a2a1821ef3a763c2143f9e
hamza <muhammadhamzasalahuddin@gmail.com>
2024-09-17 19:10:22 +0500

    removed sec

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -2,7 +2,7 @@
 const admin = require('firebase-admin');
 const dotenv = require('dotenv');
 dotenv.config();
-const serviceAccount = require('../firebase/serviceAccountKey.json');
+const serviceAccount = require('../bikeshub-9e212-firebase-adminsdk-jdu12-8680a22303.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
