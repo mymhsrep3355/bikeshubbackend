6f15893fb3d4727c8b6edc62b564ea2bc3e95afb
hamza <muhammadhamzasalahuddin@gmail.com>
2024-09-17 23:11:11 +0500

    fixes

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -2,7 +2,7 @@
 const admin = require('firebase-admin');
 const dotenv = require('dotenv');
 dotenv.config();
-const serviceAccount = require('../bikeshub-9e212-firebase-adminsdk-jdu12-8680a22303.json');
+const serviceAccount = require('../bikeshub-1c921-firebase-adminsdk-dm6ld-08c4702cc6.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
