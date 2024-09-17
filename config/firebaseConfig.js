64691f7cfb08bf2d50e99c336ffb2155d14f6172
hamza <muhammadhamzasalahuddin@gmail.com>
2024-09-17 14:17:45 +0500

    ref error fixed

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -1,10 +1,12 @@
 // firebaseConfig.js
 const admin = require('firebase-admin');
+const dotenv = require('dotenv');
+dotenv.config();
 const serviceAccount = require('./serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  storageBucket: 'gs://bikeshub-9e212.appspot.com', 
+  storageBucket: process.env.STORAGE_BUCKET 
 });
 
 const bucket = admin.storage().bucket();
