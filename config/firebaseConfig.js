ee387deb890531b01300b119cd81a0d9f13fd235
hamza <muhammadhamzasalahuddin@gmail.com>
2024-09-17 14:23:06 +0500

    git fixes

--- config/firebaseConfig.js
+++ config/firebaseConfig.js
@@ -2,7 +2,7 @@
 const admin = require('firebase-admin');
 const dotenv = require('dotenv');
 dotenv.config();
-const serviceAccount = require('./serviceAccountKey.json');
+const serviceAccount = require('../firebase/serviceAccountKey.json');
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
