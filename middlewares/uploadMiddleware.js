28481f18c9453bc00fc5e97ce0356a27842c3a45
hamza <muhammadhamzasalahuddin@gmail.com>
2024-09-21 01:13:38 +0500

    minor fixes

--- middlewares/uploadMiddleware.js
+++ middlewares/uploadMiddleware.js
@@ -1,56 +1,9 @@
-// const multer = require('multer');
-// const path = require('path');
-// const fs = require('fs');
-
-// // Ensure the uploads directory exists
-// const uploadsDir = path.join(__dirname, '../uploads');
-// if (!fs.existsSync(uploadsDir)) {
-//   fs.mkdirSync(uploadsDir, { recursive: true });
-// }
-
-// // Define storage settings for Multer
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     cb(null, uploadsDir); // Set the destination folder for uploads
-//   },
-//   filename: function (req, file, cb) {
-//     // Rename the file to include the current date to avoid conflicts
-//     cb(null, Date.now() + '-' + file.originalname);
-//   }
-// });
-
-// // Filter to only allow image files
-// const fileFilter = (req, file, cb) => {
-//   const allowedTypes = ['image/jpeg', 'image/png', 'image/gif'];
-//   if (!allowedTypes.includes(file.mimetype)) {
-//     const error = new Error('Only images are allowed');
-//     error.status = 400;
-//     return cb(error, false);
-//   }
-//   cb(null, true);
-// };
-
-// // Initialize Multer with storage settings and file filter
-// const upload = multer({
-//   storage: storage,
-//   fileFilter: fileFilter,
-//   limits: {
-//     fileSize: 1024 * 1024 * 5 // Limit file size to 5MB per file
-//   }
-// });
-
-// module.exports = upload;
-
-
-
 const multer = require('multer');
 const bucket = require('../config/firebaseConfig');
 
-//memory storage for storing files temporarily
 const storage = multer.memoryStorage();
 const upload = multer({ storage });
 
-//upload image to firebase
 const uploadImageToFirebase = async (req, res, next) => {
   if (!req.files || req.files.length === 0) {
     return res.status(400).json({ message: 'No files uploaded' });
